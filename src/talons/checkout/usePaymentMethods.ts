88d681ca81fe1908d51eee658be20ecd8c81e327
Eero Hakio <eero.hakio@ambientia.fi>
2021-09-02 20:59:35 +0300

    Change payment language field

--- src/talons/checkout/usePaymentMethods.ts
+++ src/talons/checkout/usePaymentMethods.ts
@@ -64,7 +64,7 @@ export const usePaymentMethods = () => {
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
         paymentMethod: currentSelectedPaymentMethod,
-        paymentLanguage: currentLanguage,
+        language: currentLanguage,
       }),
     };
 
