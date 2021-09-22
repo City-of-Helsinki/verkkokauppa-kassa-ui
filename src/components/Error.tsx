e4ce59ecfaeeee389099ad762cf062192d68b4fd
Aleksi Jetsu <ajetsu@gmail.com>
2021-09-22 08:02:32 +0300

    KYV-264 KassaUI-Yleinen sivuvirhesivu

--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -1,6 +1,23 @@
+import React, { useContext } from "react";
+import {
+  IconAngleLeft,
+  IconAngleRight,
+  Container,
+  Button,
+  Notification,
+} from "hds-react";
+import { useHistory } from "react-router-dom";
+import { useTranslation } from "react-i18next";
+
 export const Error = () => {
+  const { i18n, t } = useTranslation();
+
   return (
-    <h2>Error</h2>
+    <Container className="checkout-container desktop-flex">
+      <Notification className="error-notification" label={t("error.error-title")} type="error">
+      {t("error.generic-error")}
+        </Notification>
+    </Container>
   )
 }
 export default Error
