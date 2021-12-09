3afdf2b44591c66a5a35f927503e1f0fe4446cdc
severikupari1 <severikupari1@gmail.com>
2021-12-09 12:53:30 +0200

    KYV-335 : Added logic to hide accept terms checkbox and skip validation errors when order namespace matches env variable
    Signed-off-by: severikupari1 <severikupari1@gmail.com>

--- src/components/Summary.tsx
+++ src/components/Summary.tsx
@@ -14,15 +14,19 @@ import Products from "./Products";
 import { AppContext } from "../context/Appcontext";
 import { Formik, Form, Field } from "formik";
 import { getSearchParam } from "../hooks/useSearchParam";
+import { stringToArray } from "../utils/StringUtils";
 
 
 function Summary() {
   const { t } = useTranslation();
-  const { merchantTermsOfServiceUrl, orderId, firstName, lastName, email, phone, merchantUrl } = useContext(AppContext);
+  const { merchantTermsOfServiceUrl, orderId, firstName, lastName, email, phone, merchantUrl,namespace } = useContext(AppContext);
 
   const history = useHistory();
   let { id } = useParams();
 
+  let skipTermsAcceptForNamespaces = stringToArray(process.env.REACT_APP_SKIP_TERMS_ACCEPT_FOR_NAMESPACES);
+
+  const isSkipTermsAcceptForNameSpace = skipTermsAcceptForNamespaces.includes(namespace);
 
   if (!firstName) {
     history.push("/" + id);
@@ -73,15 +77,18 @@ function Summary() {
             }}
             validate={(values) => {
               const errors: any = {};
-              if (!values.acceptTerms) {
+              // skips validation for some namespaces
+              if (!isSkipTermsAcceptForNameSpace && !values.acceptTerms ) {
                 errors.acceptTerms = t("summary.terms.cb-error");
               }
+
               return errors
             }}
           >
             {({ errors, touched, isSubmitting }) => (
               <Form>
-                <Field
+
+                {isSkipTermsAcceptForNameSpace ? <></> : <Field
                   as={Checkbox}
                   id="acceptTerms"
                   type="checkbox"
@@ -95,7 +102,8 @@ function Summary() {
                       ? errors.acceptTerms
                       : undefined
                   }
-                />
+                />}
+
                 <div className="desktop-flex">
                   <Button
                     type="submit"
