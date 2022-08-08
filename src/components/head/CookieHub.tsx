fb801db5868f09bf0b18959d8e6a3d84f1706ab6
severikupari1 <severikupari1@gmail.com>
2022-08-08 12:09:12 +0300

    [SUPPORT-1534] Removed hotjar from package.json and code for hotjar initialization

--- src/components/head/CookieHub.tsx
+++ src/components/head/CookieHub.tsx
@@ -38,16 +38,7 @@ export const CookieHub = () => {
           if (this.hasConsented("analytics")) {
             //console.log('The analytics category is allowed');
             var value = 'false'
-              
-            // Uncomment if accepted_cookies event is used as trigger to start recording   
-            window.hj=window.hj||function(){(hj.q=hj.q||[]).push(arguments)};
-            hj('event', 'accepted_cookies');
-            // Uncomment if accepted_cookies event is used as trigger to start recording
-            
-            // Create a new event
-            var event = new CustomEvent('hotjar_init');
-            // Dispatch the event
-            window.dispatchEvent(event);
+             
           }else {
             //console.log('The analytics category is not allowed');
             var value = 'true'
@@ -57,39 +48,15 @@ export const CookieHub = () => {
                 document.cookie = theCookies[i].split('=')[0] + '=; path=/; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
             }
           }
-          const now = new Date();
-          var numberOfDays = 365
-          var key = '_hjOptOut'
-          
-          // set the time to be now + numberOfDays
-          now.setTime(now.getTime() + numberOfDays * 60 * 60 * 24 * 1000);
-          var expires = now.toUTCString()
-          document.cookie = key + '=' + value + '; expires=' + expires + '; path=/';
         },
         onAllow: function(category) {
           if (category == 'analytics') {
             //console.log('The analytics category was just allowed');
-              const now = new Date();
-              var numberOfDays = 365
-              var key = '_hjOptOut'
-              var value = 'false'
-              // set the time to be now + numberOfDays
-              now.setTime(now.getTime() + numberOfDays * 60 * 60 * 24 * 1000);
-              var expires = now.toUTCString()
-              document.cookie = key + '=' + value + '; expires=' + expires + '; path=/';
           }
         },
         onRevoke: function(category) {
           if (category == 'analytics') {
             //console.log('The analytics category was just revoked');
-            const now = new Date();
-            var numberOfDays = 365
-            var key = '_hjOptOut'
-            var value = 'true'
-            // set the time to be now + numberOfDays
-            now.setTime(now.getTime() + numberOfDays * 60 * 60 * 24 * 1000);
-            var expires = now.toUTCString()
-            document.cookie = key + '=' + value + '; expires=' + expires + '; path=/';
           }
         }
       };
