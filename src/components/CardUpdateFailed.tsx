bd8401b3e995132bbe542bd2cc9f7b26de4c24c5
severikupari1 <severikupari1@gmail.com>
2023-04-27 20:54:45 +0300

    [KYV-857] Refactored some code to use new paytrail update card endpoint and fetch parameters from paytrail. Added screenshots to ticket

--- src/components/CardUpdateFailed.tsx
+++ src/components/CardUpdateFailed.tsx
@@ -1,75 +1,48 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext } from "react"
 
-import { Button, Container, IconAngleRight, Notification } from "hds-react";
-import { useHistory, useParams } from "react-router-dom";
-import { useTranslation } from "react-i18next";
+import { Button, Container, IconAngleRight, Notification } from "hds-react"
+import { useHistory, useParams } from "react-router-dom"
+import { useTranslation } from "react-i18next"
 import { AppContext } from "../context/Appcontext"
-import { useUpdateCard } from "../talons/checkout/useUpdateCard"
+import { useUpdateCardFormParameters } from "../talons/checkout/useUpdateCardFormParameters"
 
 
 function CardUpdateFailed() {
-  const { t } = useTranslation();
+  const { t } = useTranslation()
 
-  const { orderId, subscriptionId} = useContext(AppContext);
+  const { orderId } = useContext(AppContext)
+  const {
+    redirectToPaytrailUpdateCardForm,
+  } = useUpdateCardFormParameters()
 
-  const history = useHistory();
+  const history = useHistory()
   if (!orderId) {
-      history.push("/");
+    history.push("/")
   }
 
-  const { fetchUpdateCardPaymentUrl, loading: updateCardPaymentUrlLoading } = useUpdateCard();
-  const [, setUpdateCardUrlLoading] = useState(true);
-
-  let { id } = useParams();
-
-  const [updateCardPaymentUrl, setUpdateCardPaymentUrl] = useState("");
-
-  const handleProceedToUpdateCard = () => {
-    if (updateCardPaymentUrl) {
-      window.location.href = updateCardPaymentUrl;
-    }
-  };
+  let { id } = useParams()
 
   const handleProceedToSuccess = () => {
     if (id) {
-      window.location.href = "/"+id+"/success";
+      window.location.href = `/${ id }/success`
     }
-  };
-
-  useEffect(() => {
-    setUpdateCardUrlLoading(false)
-
-    if (subscriptionId) {
-      fetchUpdateCardPaymentUrl(subscriptionId).then((data) => {
-        if (updateCardPaymentUrlLoading) {
-          setUpdateCardUrlLoading(true)
-          return
-        }
-        if (null !== data) {
-          setUpdateCardPaymentUrl(data.paymentUrl)
-        } 
-
-        setUpdateCardUrlLoading(false)
-      });
-    }
-    // eslint-disable-next-line
-  }, [id, orderId]);
+  }
 
   return (
     <div className="App2">
       <Container className="checkout-container success" id="checkout-container">
 
-      <Notification className="success-notification" label={t("update-card.card-update-failed-header")} type="alert">{t("update-card.card-update-failed-content")}</Notification>
+        <Notification className="success-notification" label={ t("update-card.card-update-failed-header") }
+                      type="alert">{ t("update-card.card-update-failed-content") }</Notification>
 
-      <div className="checkout-actions full-width-button">
+        <div className="checkout-actions full-width-button">
           <Button
             type="submit"
             className="submit"
-            onClick={handleProceedToUpdateCard}
-            iconRight={<IconAngleRight className={'icon-right'}/>}
-            disabled={updateCardPaymentUrlLoading}
+            onClick={ () => redirectToPaytrailUpdateCardForm(orderId) }
+            iconRight={ <IconAngleRight className={ 'icon-right' }/> }
           >
-           {t("update-card.card-update-failed-try-again")}
+            { t("update-card.card-update-failed-try-again") }
           </Button>
         </div>
 
@@ -78,17 +51,17 @@ function CardUpdateFailed() {
             type="submit"
             className="submit"
             variant="secondary"
-            onClick={handleProceedToSuccess}
-            iconRight={<IconAngleRight className={'icon-right'}/>}
+            onClick={ handleProceedToSuccess }
+            iconRight={ <IconAngleRight className={ 'icon-right' }/> }
           >
-           {t("update-card.go-to-receipt")}
+            { t("update-card.go-to-receipt") }
           </Button>
 
-          
+
         </div>
       </Container>
     </div>
-  );
+  )
 }
 
-export default CardUpdateFailed;
+export default CardUpdateFailed
