4a7f91d880337510f03e335dfdf1b8cefeeff268
Jarkko Laukkanen <jarkko.laukkanen@ambientia.fi>
2026-04-29 13:44:59 +0300

    [KYV-1330] step numbers not read alout by screenreaders, use separate hidden text instead

--- src/components/layout/steps/Steps.tsx
+++ src/components/layout/steps/Steps.tsx
@@ -1,5 +1,6 @@
-import React from "react"
-import { Container } from "hds-react-next"
+import React from "react";
+import { Container } from "hds-react-next";
+import { useTranslation } from "react-i18next";
 
 interface Props {
   statusLabel: string;
@@ -9,33 +10,39 @@ interface Props {
 
 function Steps(props: Props) {
   const { activeStep, steps } = props;
+  const { t } = useTranslation();
+
   return (
     <div className="light-bg">
-    <Container className="checkout-container">
-      <div className="steps">
-        <div className="status-label">
-          <h1>{props.statusLabel}</h1>
-        </div>
-        
-        <div className="steps-container">
-          {[...Array(steps)].map((e, step) => (
-            <div className="step-container" key={`step-${step + 1}`}>
-              <div
-                className={
-                  (activeStep === step + 1 && "step active") ||
-                  (activeStep > step + 1 && "step done") ||
-                  "step"
-                }
-                id={`step-${step + 1}`}
-                key={`step-${step + 1}-div`}
-              >
-                {activeStep <= step + 1 && step + 1}
-              </div>
+      <Container className="checkout-container">
+        <div className="steps">
+          <div className="status-label">
+            <h1>{props.statusLabel}</h1>
+          </div>
+
+          <ol className="steps-container">
+            <div aria-live="polite" className="sr-only">
+              {`${t("steps.step")} ${activeStep} / ${steps}: ${props.statusLabel}`}
             </div>
-          ))}
+            {[...Array(steps)].map((e, step) => (
+              <li className="step-container" key={`step-${step + 1}`}>
+                <div
+                  className={
+                    (activeStep === step + 1 && "step active") ||
+                    (activeStep > step + 1 && "step done") ||
+                    "step"
+                  }
+                  id={`step-${step + 1}`}
+                  key={`step-${step + 1}-div`}
+                  aria-hidden="true"
+                >
+                  {activeStep <= step + 1 && step + 1}
+                </div>
+              </li>
+            ))}
+          </ol>
         </div>
-      </div>
-    </Container>
+      </Container>
     </div>
   );
 }
